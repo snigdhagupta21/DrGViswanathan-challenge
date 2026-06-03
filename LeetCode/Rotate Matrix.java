eb2fd72731b46100a646e84433e206f223b76fc9
snigdhagupta21 <guptasnigdha826@gmail.com>
2026-06-04 00:05:51 +0530

    Create Rotate Matrix.java

---
+++ LeetCode/Rotate Matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix (swap rows and columns)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Swap matrix[i][j] and matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            reverseRow(matrix[i]);
+        }
+    }
+
+    private void reverseRow(int[] row) {
+        int start = 0, end = row.length - 1;
+        while (start < end) {
+            int temp = row[start];
+            row[start] = row[end];
+            row[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
