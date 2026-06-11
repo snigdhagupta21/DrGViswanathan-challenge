001b7d0ad4150ceb73200b69534a95dd044e5d70
snigdhagupta21 <guptasnigdha826@gmail.com>
2026-06-12 00:09:15 +0530

    Create Add two numbers as LinkedList.java

---
+++ LeetCode/Add two numbers as LinkedList.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode current=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null){
+            int val1=(l1!=null)?l1.val:0;
+            int val2=(l2!=null)?l2.val:0;
+            int sum=val1+val2+carry;
+            carry=sum/10;
+            current.next=new ListNode(sum%10);
+            if(l1!=null){
+                l1=l1.next;
+            }
+            if(l2!=null){
+                l2=l2.next;
+            }
+            current=current.next;
+
+        }
+        if(carry>0){
+            current.next=new ListNode(carry);
+        }
+        return dummy.next;
+        
+    }
+}
