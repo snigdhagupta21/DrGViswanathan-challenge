3eda580e4b9aafb975df8e500ca2ba303dc35e7c
snigdhagupta21 <guptasnigdha826@gmail.com>
2026-06-12 00:08:42 +0530

    Create Remove N-th node from back of LinkedList.java

---
+++ LeetCode/Remove N-th node from back of LinkedList.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow=head;
+        ListNode fast=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            return head.next;
+        }
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+
+    }
+}
