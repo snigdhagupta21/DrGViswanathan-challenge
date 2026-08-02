a0a933aa337bde36acefe685c1e08ade7772355b
snigdhagupta21 <guptasnigdha826@gmail.com>
2026-08-02 23:46:20 +0530

    Create Bottom View of Binary Tree.java

---
+++ GeeksForGeeks/Bottom View of Binary Tree.java
@@ -0,0 +1,36 @@
+class Solution {
+    class Pair{
+        Node node;
+        int hd;
+        Pair(Node node, int hd){
+            this.node=node;
+            this.hd=hd;
+        }
+    }
+    public ArrayList<Integer> bottomView(Node root) {
+        // code here
+        ArrayList<Integer> result=new ArrayList<>();
+        Queue<Pair> queue= new LinkedList<>();
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        queue.add(new Pair(root,0));
+        if(root==null){
+            return result;
+        }
+        while(!queue.isEmpty()){
+            Pair curr= queue.poll();
+            Node node= curr.node;
+            int hd= curr.hd;
+            map.put(hd,node.data);
+            if(node.left !=null){
+                queue.add(new Pair(node.left,hd-1));
+            }
+            if(node.right !=null){
+                queue.add(new Pair(node.right,hd+1));
+            }
+        }
+        for(int val: map.values()){
+            result.add(val);
+        }
+        return result;
+    }
+}
