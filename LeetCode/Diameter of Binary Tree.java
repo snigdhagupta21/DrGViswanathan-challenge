68f9bce6af735c203657df560f77dad5b5793891
snigdhagupta21 <guptasnigdha826@gmail.com>
2026-08-11 23:41:12 +0530

    Create Diameter of Binary Tree.java

---
+++ LeetCode/Diameter of Binary Tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    private int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return diameter;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftDepth = dfs(node.left);
+        int rightDepth = dfs(node.right);
+
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
