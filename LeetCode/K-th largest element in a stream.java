9d4a0067de883a067e1735b7604a0d177ff634c6
snigdhagupta21 <guptasnigdha826@gmail.com>
2026-08-12 23:43:09 +0530

    Create K-th largest element in a stream.java

---
+++ LeetCode/K-th largest element in a stream.java
@@ -0,0 +1,28 @@
+class KthLargest {
+
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k = k;
+        pq = new PriorityQueue<>();
+
+        for(int num : nums){
+            add(num);  
+        }
+    }
+
+    public int add(int val) {
+
+        pq.add(val);
+
+        if(pq.size() > k){
+            pq.poll();   
+        }
+
+        return pq.peek(); 
+    }
+}
+
+
